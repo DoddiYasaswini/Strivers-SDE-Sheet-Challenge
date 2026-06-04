b997517c09e986c353f0fe40f8b1e8f167008319
doddiyasaswini <doddiyasaswini686@gmail.com>
2026-06-04 14:46:57 +0530

    Implement number of inversions in an array

---
+++ Day04/Inversion_of_Array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+   long long int numberOfInversions(vector<int> nums) {
+    int n=nums.size();
+    long long count=0;
+    for(int i=0;i<n;i++){
+    for(int j=i+1;j<n;j++){
+        if(nums[j]<nums[i]){
+           count++;
+        }
+    }
+}
+  return count;
+    }
+};
