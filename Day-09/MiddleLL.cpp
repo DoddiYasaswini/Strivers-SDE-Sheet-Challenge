6e252350840dac34c915919c1bc0da8391313161
doddiyasaswini <doddiyasaswini686@gmail.com>
2026-06-09 21:38:37 +0530

    Implement function to find the middle of a linked list

---
+++ Day-09/MiddleLL.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        vector<int>res;
+        ListNode *temp=head;
+        while(temp!=nullptr){
+            res.push_back(temp->val);
+            temp=temp->next;
+        }
+        int n=res.size();
+        temp=head;
+        for(int i=0;i<n/2;i++){
+            temp=temp->next;
+        }
+        head=temp;
+        return head;
+    }
+};
