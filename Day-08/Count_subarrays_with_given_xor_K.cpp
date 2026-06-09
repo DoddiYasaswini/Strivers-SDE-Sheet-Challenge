117eae1956b20071dc272ee5c5bd9992efe6270c
doddiyasaswini <doddiyasaswini686@gmail.com>
2026-06-09 19:10:55 +0530

    Create Count_subarrays_with_given_xor_K.cpp

---
+++ Day-08/Count_subarrays_with_given_xor_K.cpp
@@ -0,0 +1,16 @@
+class Solution{
+public:
+    int subarraysWithXorK(vector<int> &nums, int k) {
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=i;j<n;j++){
+                sum=sum^nums[j];
+                if(sum==k)
+                count++;
+            }
+        }
+        return count;
+    }
+};
